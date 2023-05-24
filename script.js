61c858480d5318f286c305d1e586085c2b782f12
Harshil Patel <120940967+harxxhilgg@users.noreply.github.com>
2023-05-25 01:51:25 +0530

    app done, still minor bugs

---
+++ script.js
@@ -0,0 +1,40 @@
+var billAmount = document.getElementById('billAmount');
+var cashGiven = document.getElementById('cashGiven');
+var noOfNotes = document.querySelectorAll('.noOfNotes');
+var checkBtn = document.getElementById('checkBtn');
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+checkBtn.addEventListener('click', () => {
+    hideError();
+    if (billAmount.value > 0 ) { 
+        if (cashGiven.value >= billAmount.value) {
+            var cashCounter = cashGiven.value - billAmount.value;
+            calculateCounter(cashCounter);
+        }
+        else {
+            showError();
+        }
+    }
+    else { 
+        showError();
+    }
+})
+
+function showError() { 
+    document.querySelector('.errorOutput').innerHTML = "INVALID INPUT!";
+}
+
+function hideError() { 
+    document.querySelector('.errorOutput').innerHTML = "";
+}
+
+function calculateCounter(cashCounter) { 
+    for (let i = 0; i < availableNotes.length; i++) {
+        var numberOfNotes = Math.trunc(cashCounter / availableNotes[i]);
+
+        cashCounter %= availableNotes[i];
+
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
